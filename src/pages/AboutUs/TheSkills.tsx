dad60a87302ebe6ac52deb8f2b1a3c470c7f61d8
savagegraphics <olawaleraji188@gmail.com>
2023-08-07 04:19:12 -0700

    first commit

--- src/pages/AboutUs/TheSkills.tsx
+++ src/pages/AboutUs/TheSkills.tsx
@@ -1,96 +1,92 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
 const TheSkills = (props: Props) => {
   return (
-
-
-<div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-  <div className="max-w-2xl text-center mx-auto mb-10 lg:mb-14">
-    <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Read our latest news</h2>
-    <p className="mt-1 text-gray-600 dark:text-gray-400">We&apos;ve helped some great companies brand, design and get to market.</p>
-  </div>
-
-  <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-10 lg:mb-14">
-    <Link className="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="#">
-      <div className="aspect-w-16 aspect-h-9">
-        <Image className="w-full object-cover rounded-t-xl" src="https://images.unsplash.com/photo-1668869713519-9bcbb0da7171?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80" alt="Image Description"  width={200} height={100}/>
-      </div>
-      <div className="p-4 md:p-5">
-        <p className="mt-2 text-xs uppercase text-gray-600 dark:text-gray-400">
-          Product
+    <div className='mr-12 ml-12'>
+      <div className="flex items-center mb-5">
+        <p className="bg-blue-100 text-blue-800 text-sm font-semibold inline-flex items-center p-1.5 rounded dark:bg-blue-200 dark:text-blue-800">
+          8.7
         </p>
-        <h3 className="mt-2 text-lg font-medium text-gray-800 group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
-          Better is when everything works together
-        </h3>
+        <p className="ml-2 font-medium text-gray-900 dark:text-white">Excellent</p>
+        <span className="w-1 h-1 mx-2 bg-gray-900 rounded-full dark:bg-gray-500"></span>
+        <p className="text-sm font-medium text-gray-500 dark:text-gray-400">376 reviews</p>
+        <a href="#" className="ml-auto text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+          Read all reviews
+        </a>
       </div>
-    </Link>
- 
-    <Link className="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="#">
-      <div className="aspect-w-16 aspect-h-9">
-        <Image className="w-full object-cover rounded-t-xl" src="https://images.unsplash.com/photo-1668584054035-f5ba7d426401?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3465&q=80" alt="Image Description"  width={200} height={100}/>
-      </div>
-      <div className="p-4 md:p-5">
-        <p className="mt-2 text-xs uppercase text-gray-600 dark:text-gray-400">
-          Business
-        </p>
-        <h3 className="mt-2 text-lg font-medium text-gray-800 group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
-          What CFR really is about
-        </h3>
-      </div>
-    </Link>
-
-    <Link className="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="#">
-      <div className="aspect-w-16 aspect-h-9">
-        <Image className="w-full object-cover rounded-t-xl" src="https://images.unsplash.com/photo-1668863699009-1e3b4118675d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3387&q=80" alt="Image Description"  width={200} height={100}/>
-      </div>
-      <div className="p-4 md:p-5">
-        <p className="mt-2 text-xs uppercase text-gray-600 dark:text-gray-400">
-          Business
-        </p>
-        <h3 className="mt-2 text-lg font-medium text-gray-800 group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
-          Should Product Owners think like entrepreneurs?
-        </h3>
-      </div>
-    </Link>
-
-    <Link className="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="#">
-      <div className="aspect-w-16 aspect-h-9">
-        <Image className="w-full object-cover rounded-t-xl" src="https://images.unsplash.com/photo-1668584054131-d5721c515211?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="Image Description"  width={200} height={100}/>
-      </div>
-      <div className="p-4 md:p-5">
-        <p className="mt-2 text-xs uppercase text-gray-600 dark:text-gray-400">
-          Facilitate
-        </p>
-        <h3 className="mt-2 text-lg font-medium text-gray-800 group-hover:text-blue-600 dark:text-gray-300 dark:group-hover:text-white">
-          Announcing Front Strategies: Ready-to-use rules
-        </h3>
-      </div>
-    </Link>
-  </div>
-
-
-  <div className="text-center">
-    <div className="inline-block bg-white border shadow-sm rounded-full dark:bg-slate-900 dark:border-gray-800">
-      <div className="py-3 px-4 flex items-center gap-x-2">
-        <p className="text-gray-600 dark:text-gray-400">
-          Want to read more?
-        </p>
-        <Link className="inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 hover:underline font-medium" href="../docs/index.html">
-          Go here
-          <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
-            <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
-          </svg>
-        </Link>
+      <div className="gap-8 sm:grid sm:grid-cols-2">
+        <div>
+          <dl>
+            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Staff</dt>
+            <dd className="flex items-center mb-3">
+              <div className="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style={{ width: '88%' }}></div>
+              </div>
+              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">8.8</span>
+            </dd>
+          </dl>
+          <dl>
+            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Comfort</dt>
+            <dd className="flex items-center mb-3">
+              <div className="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style={{ width: '89%' }}></div>
+              </div>
+              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">8.9</span>
+            </dd>
+          </dl>
+          <dl>
+            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Free WiFi</dt>
+            <dd className="flex items-center mb-3">
+              <div className="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style={{ width: '88%' }}></div>
+              </div>
+              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">8.8</span>
+            </dd>
+          </dl>
+          <dl>
+            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Facilities</dt>
+            <dd className="flex items-center">
+              <div className="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style={{ width: '54%' }}></div>
+              </div>
+              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">5.4</span>
+            </dd>
+          </dl>
+        </div>
+        <div>
+          <dl>
+            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Value for money</dt>
+            <dd className="flex items-center mb-3">
+              <div className="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style={{ width: '89%' }}></div>
+              </div>
+              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">8.9</span>
+            </dd>
+          </dl>
+          <dl>
+            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Cleanliness</dt>
+            <dd className="flex items-center mb-3">
+              <div className="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style={{ width: '70%' }}></div>
+              </div>
+              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">7.0</span>
+            </dd>
+          </dl>
+          <dl>
+            <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Location</dt>
+            <dd className="flex items-center">
+              <div className="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style={{ width: '89%' }}></div>
+              </div>
+              <span className="text-sm font-medium text-gray-500 dark:text-gray-400">8.9</span>
+            </dd>
+          </dl>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default TheSkills
-
+export default TheSkills;
