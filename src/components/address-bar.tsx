7ba7fb7391d2e3e0addecbddce5bdda21c81b595
savagegraphics <olawaleraji188@gmail.com>
2023-08-25 06:29:46 -0700

    first commit

--- src/components/address-bar.tsx
+++ src/components/address-bar.tsx
@@ -16,13 +16,13 @@ export function AddressBar() {
           </Link>
           <Link
             className="flex rounded px-2 py-2.5 text-gray-500 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
-            href="/about"
+            href="/About"
           >
             <span className="font-medium">About</span>
           </Link>
           <Link
             className="flex rounded px-2 py-2.5 text-gray-500 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
-            href="/resume"
+            href="/Resume"
           >
             <span className="font-medium">Resume</span>
           </Link>
