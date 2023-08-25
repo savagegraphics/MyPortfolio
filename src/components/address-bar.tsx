fd826dbcf65aa1da5c059554a8dbf2e361df531a
savagegraphics <olawaleraji188@gmail.com>
2023-08-25 09:24:16 -0700

    first commit

--- src/components/address-bar.tsx
+++ src/components/address-bar.tsx
@@ -9,31 +9,31 @@ export function AddressBar() {
       <div className="flex gap-x-1 text-sm font-medium">
         <div className="flex">
           <Link
-            className="flex rounded px-2 py-2.5 text-gray-500 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+            className="flex rounded px-2 py-2.5 text-gray-300 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
             href="/"
           >
             <span className="font-medium">Dashboard</span>
           </Link>
           <Link
-            className="flex rounded px-2 py-2.5 text-gray-500 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+            className="flex rounded px-2 py-2.5 text-gray-300 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
             href="/About"
           >
             <span className="font-medium">About</span>
           </Link>
           <Link
-            className="flex rounded px-2 py-2.5 text-gray-500 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+            className="flex rounded px-2 py-2.5 text-gray-300 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
             href="/Resume"
           >
             <span className="font-medium">Resume</span>
           </Link>
           <Link
-            className="flex rounded px-2 py-2.5 text-gray-500 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+            className="flex rounded px-2 py-2.5 text-gray-300 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
             href="/portfolio"
           >
             <span className="font-medium">Portfolio</span>
           </Link>
           <Link
-            className="flex rounded px-2 py-2.5 text-gray-500 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+            className="flex rounded px-2 py-2.5 text-gray-300 transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
             href="/contact"
           >
             <span className="font-medium">Contact</span>
