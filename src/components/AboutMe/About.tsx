b2dac28f4e96b63980856754dbc93691ccd79829
Olawale Raji Toheeb <100351835+savagegraphics@users.noreply.github.com>
2023-08-26 02:20:33 -0500

    Update About.tsx

--- src/components/AboutMe/About.tsx
+++ src/components/AboutMe/About.tsx
@@ -65,19 +65,6 @@ const About = () => {
             </aside>
           </div>
         </article>
-        <div className="grid items-center lg:grid-cols-2 gap-6 lg:gap-16">
-        <div className="flex flex-col p-4 m-8">
-        {/* <OtherSkills/> */}
-         </div>
-         <div className="flex flex-col p-4">
-         {/* <h1 className='m-8 text-2xl font-serif text-gray-200'>Additional Expertise.</h1> */}
-         {/* <Accordion/> */}
-         </div>
-         <div className="flex flex-col p-4">
-         </div>
-         <div className="flex flex-col p-4">
-         </div>
-        </div>
       </section>
     </div>
   )
