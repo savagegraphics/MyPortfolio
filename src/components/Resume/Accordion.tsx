8ca0b11ebfdd6df123e31fdba9efb8094bf17b5c
savagegraphics <olawaleraji188@gmail.com>
2023-08-25 21:18:02 -0700

    first commit

--- src/components/Resume/Accordion.tsx
+++ src/components/Resume/Accordion.tsx
@@ -4,23 +4,22 @@ type Props = {}
 
 const Accordion = (props: Props) => {
   return (
-<section className="dark:bg-gray-800 dark:text-gray-100">
-<h1 className='m-8 text-2xl font-serif text-gray-200'>Additional Expertise.</h1>
-	<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+<section className="bg-gray-800 text-gray-100">
+<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
 		<h2 className="text-2xl font-semibold sm:text-4xl">Frequently Asked Questions</h2>
 		<p className="mt-4 mb-8 dark:text-gray-400">Sagittis tempor donec id vestibulum viverra. Neque condimentum primis orci at lacus amet bibendum.</p>
 		<div className="space-y-4">
 			<details className="w-full border rounded-lg">
 				<summary className="px-4 py-6 focus:outline-none focus-visible:ri">Problem-Solving</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">The ability to analyze challenges and find creative, efficient solutions for both technical and design issues.</p>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-400">The ability to analyze challenges and find creative, efficient solutions for both technical and design issues.</p>
 			</details>
 			<details className="w-full border rounded-lg">
 				<summary className="px-4 py-6 focus:outline-none focus-visible:ri">Lorem at arcu rutrum viverra metus sapien venenatis lobortis odio?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-400">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
 			</details>
 			<details className="w-full border rounded-lg">
 				<summary className="px-4 py-6 focus:outline-none focus-visible:ri">Eleifend feugiat sollicitudin laoreet adipiscing bibendum suscipit erat?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-400">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
 			</details>
 		</div>
 	</div>
--- src/components/Resume/ResumeList.tsx
+++ src/components/Resume/ResumeList.tsx
@@ -41,55 +41,49 @@ const ResumeList = (props: Props) => {
       <section className='bg-gray-800 lg:mt-4 m-4 lg:mx-16 rounded-3xl lg-h-[650px] md-h-[580px] sm-h-[700px] max-w-[85rem] px-4 sm:px-6 lg:px-8'>
         <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
           <div className="flex py-4 overflow-x-auto overflow-y-hidden md:justify-center dark:border-gray-700">
-            <Link href="/Resume">
               <button
                 className={`h-12 px-8 py-2 -mb-px text-sm text-center ${
                   activeButton === 'Animation'
-                    ? 'text-blue-600 border-b-2 border-blue-500'
-                    : 'text-gray-700 border-b-2 border-gray-200'
+                    ? 'text-indigo-300 border-b-2 border-indigo-500'
+                    : 'text-gray-200 border-b-2 border-gray-200'
                 } sm:text-base dark:text-white whitespace-nowrap cursor-pointer focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400`}
                 onClick={() => handleButtonClick('Animation')}
               >
                 Experience
               </button>
-            </Link>
 
-            <Link href="/OtherSkills"><button
+          <button
               className={`h-12 px-8 py-2 -mb-px text-sm text-center ${
                 activeButton === 'Web Design'
-                  ? 'text-blue-600 border-b-2 border-blue-500'
-                  : 'text-gray-700 border-b-2 border-gray-200'
-              } sm:text-base dark:text-white whitespace-nowrap cursor-pointer focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400`}
+                ? 'text-indigo-300 border-b-2 border-indigo-500'
+                : 'text-gray-200 border-b-2 border-gray-200'
+             } sm:text-base dark:text-white whitespace-nowrap cursor-pointer focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400`}
               onClick={() => handleButtonClick('Web Design')}
             >
               Skills
-            </button> </Link>
+            </button>
 
-            <Link href="/OtherSkills">
               <button
                 className={`h-12 px-8 py-2 -mb-px text-sm text-center ${
                   activeButton === 'App Design'
-                    ? 'text-blue-600 border-b-2 border-blue-500'
-                    : 'text-gray-700 border-b-2 border-gray-200'
-                } sm:text-base dark:text-white whitespace-nowrap cursor-pointer focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400`}
+                  ? 'text-indigo-300 border-b-2 border-indigo-500'
+                  : 'text-gray-200 border-b-2 border-gray-200'
+              } sm:text-base dark:text-white whitespace-nowrap cursor-pointer focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400`}
                 onClick={() => handleButtonClick('App Design')}
               >
                 Additional Expertise.
               </button>
-            </Link>
 
-            <Link href="/Resume">
               <button
                 className={`h-12 px-8 py-2 -mb-px text-sm text-center ${
                   activeButton === 'Branding'
-                    ? 'text-blue-600 border-b-2 border-blue-500'
-                    : 'text-gray-700 border-b-2 border-gray-200'
-                } sm:text-base dark:text-white whitespace-nowrap cursor-pointer focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400`}
+                  ? 'text-indigo-300 border-b-2 border-indigo-500'
+                  : 'text-gray-200 border-b-2 border-gray-200'
+              } sm:text-base dark:text-white whitespace-nowrap cursor-pointer focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400`}
                 onClick={() => handleButtonClick('Branding')}
               >
                 Branding
               </button>
-            </Link>
           </div>
           {/* Display the dynamic content */}
           <div className="mt-8">
